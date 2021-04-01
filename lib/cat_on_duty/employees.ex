b036a17c092cdd446641cce06a8ac98abc83859a
Pavel Solovev <psolvy@gmail.com>
2021-04-01 11:22:36 +0300

    fix blank duty at sentry

--- lib/cat_on_duty/employees.ex
+++ lib/cat_on_duty/employees.ex
@@ -124,7 +124,15 @@ defmodule CatOnDuty.Employees do
     |> where([s], s.team_id == ^team_id)
     |> where([s], not s.on_vacation?)
     |> Repo.all()
-    |> Enum.sort_by(&Date.to_erl(&1.last_duty_at))
+    |> Enum.sort_by(&last_duty_at(&1))
+  end
+
+  @spec last_duty_at(Sentry.t()) :: :calendar.date() | nil
+  defp last_duty_at(%Sentry{} = sentry) do
+    case sentry.last_duty_at do
+      nil -> nil
+      date -> Date.to_erl(date)
+    end
   end
 
   @spec get_sentry!(pos_integer) :: Sentry.t()
